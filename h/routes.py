67ef949dd6927f9987e0b9444b7b2c351e15e718
Chapman Snowden <Chapman.Snowden@gmail.com>
2018-06-19 17:36:59 -0500

    remove redundant privacy route

--- h/routes.py
+++ h/routes.py
@@ -186,7 +186,7 @@ def includeme(config):
         'hypothesis-twitter', 'https://twitter.com/hypothes_is', static=True)
     config.add_route('jobs', '/jobs/', static=True)
     config.add_route('press', '/press/', static=True)
-    config.add_route('privacy', '/privacy/', static=True)
+    #config.add_route('privacy', '/privacy/', static=True)
     config.add_route('roadmap', '/roadmap/', static=True)
     config.add_route('team', '/team/', static=True)
     config.add_route('terms-of-service', '/terms-of-service/', static=True)
